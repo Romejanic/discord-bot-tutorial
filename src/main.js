4025f2dcb950755de1234a0f2f6520bc65226889
Jack Davenport <jack.davenport5011@gmail.com>
2021-10-02 12:20:00 +1000

    Added basic bot code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log("hello world!");
+const { SlasherClient } = require("discord.js-slasher");
+
+const client = new SlasherClient({ useAuth: true });
+
+client.on("command", (ctx) => {
+    if(ctx.name === "test") {
+        ctx.reply("Hello world!");
+    }
+});
+
+client.on("ready", () => {
+    console.log("I'm logged in!"); 
+});
+
+client.login();
