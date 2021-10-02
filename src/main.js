e9def38e3a81ef9c385beaee4dd6216d60e1e215
Jack Davenport <jack.davenport5011@gmail.com>
2021-10-02 14:09:23 +1000

    Added /greet command

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ const { SlasherClient } = require("discord.js-slasher");
 const client = new SlasherClient({ useAuth: true });
 
 client.on("command", (ctx) => {
-    if(ctx.name === "test") {
-        ctx.reply("Hello world!");
+    if(ctx.name === "greet") {
+        // gets user, or target the sender if no user given
+        let target = ctx.options.getUser("target") || ctx.user;
+
+        // target.toString() creates a mention
+        ctx.reply("Hello there, " + target.toString() + "! :D");
     }
 });
 
